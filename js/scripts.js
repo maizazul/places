a39cbd07d18e8874c2cf830c5108ef7547aff3b7
Danny Pineda and evan stewart <stewarea@gmail.com>
2016-03-16 19:25:26 -0700

    return place with link

--- js/scripts.js
+++ js/scripts.js
@@ -10,14 +10,19 @@ function Location (place, location, landmark, time, fun, notes) {
 
 
 $(document).ready(function()  {
-  $("form#new-location").submit(function(event) {
+  $("form#new-location").submit(function(event)  {
     event.preventDefault();
     var inputPlace = $("input#new-place").val();
+    var inputLocation = $("input#place-location").val();
+    var inputDetails = $("input#place-details").val();
+    var inputTime = $("input#place-time").val();
+    var inputFun = $("input#place-fun").val();
+    var inputNotes = $("input#place-notes").val();
 
-    var inputPlace = $("input#place-location").val();
-    var inputPlace = $("input#place-details").val();
-    var inputPlace = $("input#place-time").val();
-    var inputPlace = $("input#place-fun").val();
-    var inputPlace = $("input#place-notes").val();
-
-    $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li");
+    $("ul#list").append("<li><span class='listplace'>" + inputPlace + "</span></li");
+    $(".listplace").last().click(function() {
+      $("show-details").show();
+      $("show-details").text(inputPlace);
+    });
+  });
+});
