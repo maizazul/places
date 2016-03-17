4d220cbab23bfae4c351902f92ea08b5026dbe1a
Danny Pineda and evan stewart <stewarea@gmail.com>
2016-03-16 20:53:17 -0700

    show some fields

--- js/scripts.js
+++ js/scripts.js
@@ -7,22 +7,43 @@ function Location (place, location, landmark, time, fun, notes) {
   this.notes = notes;
 }
 
+Location.prototype.fullDetails = function() {
+  return this.place + ", " + this.location + ", " + this.landmark + ", " + this.time + ", " + this.fun + ", " + this.notes;
+}
+
 
 
 $(document).ready(function()  {
   $("form#new-location").submit(function(event)  {
     event.preventDefault();
-    var inputPlace = $("input#new-place").val();
-    var inputLocation = $("input#place-location").val();
-    var inputDetails = $("input#place-details").val();
-    var inputTime = $("input#place-time").val();
-    var inputFun = $("input#place-fun").val();
-    var inputNotes = $("input#place-notes").val();
 
-    $("ul#list").append("<li><span class='listplace'>" + inputPlace + "</span></li");
+    var inputPlace = $(this).find("input#new-place").val();
+    var inputLocation = $(this).find("input#place-location").val();
+    var inputLandmarks = $(this).find("input#place-landmarks").val();
+    var inputTime = $(this).find("input#place-time").val();
+    var inputFun = $(this).find("input#place-fun").val();
+    var inputNotes = $(this).find("input#place-notes").val();
+    var newPlace = new Location(inputPlace);
+    var newDetails = new Location(inputLocation, inputLandmarks, inputTime, inputFun, inputNotes);
+    // inputPlace.push(newDetails);
+
+    $("ul#list").append("<li><span class='listplace'>" + newPlace.place + "</span></li");
     $(".listplace").last().click(function() {
-      $("show-details").show();
-      $("show-details").text(inputPlace);
+
+      $("#show-details").show();
+      $("#show-details h2").text(newPlace.place);
+      $(".place").text(inputPlace);
+      $(".location").text(inputLocation);
+      $(".landmarks").text(inputLandmarks);
+      $(".time").text(inputTime);
+      $(".fun").text(inputFun);
+      $(".notes").text(inputNotes);
+        console.log("click");
+
+      // $("ul#info").text("");
+      //   $("ul#addresses").append("<li>" + fullDetails() + "</li>");
+
+
     });
   });
 });
